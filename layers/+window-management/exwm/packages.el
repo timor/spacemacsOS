72ec9edb288793252b8edd54b2437cf49e5ba52b
timor <timor.dd@googlemail.com>
2017-09-19 23:38:08 +0200

    Add a helper for easy binding of starting and switching to EXWM windows

--- layers/+window-management/exwm/packages.el
+++ layers/+window-management/exwm/packages.el
@@ -63,6 +63,25 @@
         (setq key     (pop bindings)
               command (pop bindings))))
 
+    (defun exwm-switch-to-buffer-or-run (window-class command)
+      "Switch to first buffer with window-class, and if not present, run command."
+      (let ((buffer
+             (find window-class (buffer-list) :key (lambda(b) (cdr (assoc 'exwm-class-name (buffer-local-variables b)))) :test 'string-equal)))
+        (if buffer
+            (exwm-workspace-switch-to-buffer buffer)
+          (start-process-shell-command command nil command))))
+
+    ;; Can be used to bind a key to jumping to an application, or alternatively starting it.  E.g.:
+    ;;
+    ;; (spacemacs/exwm-bind-switch-to-or-run-command "s-f" "Firefox" "firefox")
+    ;;
+    ;; The window class can be found out with exwm's builtin info functions, but for most applications it should just match the buffer name.
+    (defun spacemacs/exwm-bind-switch-to-or-run-command (key window-class command)
+      (exwm-input-set-key (kbd key)
+                          `(lambda ()
+                             (interactive)
+                             (exwm-switch-to-buffer-or-run ,window-class ,command))))
+
     (spacemacs/exwm-bind-command
      "<s-return>"  exwm--terminal-command)
 
