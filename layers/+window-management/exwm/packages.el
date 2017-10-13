2f59bb86c7c907b150d4749a6070f68563f4992f
timor <timor.dd@googlemail.com>
2017-10-13 10:35:17 +0200

    start phasing out functions into own file

--- layers/+window-management/exwm/packages.el
+++ layers/+window-management/exwm/packages.el
@@ -116,17 +116,6 @@
             (exwm-workspace-switch-to-buffer buffer)
           (start-process-shell-command command nil command))))
 
-    ;; Can be used to bind a key to jumping to an application, or alternatively starting it.  E.g.:
-    ;;
-    ;; (spacemacs/exwm-bind-switch-to-or-run-command "s-f" "Firefox" "firefox")
-    ;;
-    ;; The window class can be found out with exwm's builtin info functions, but for most applications it should just match the buffer name.
-    (defun spacemacs/exwm-bind-switch-to-or-run-command (key window-class command)
-      (exwm-input-set-key (kbd key)
-                          `(lambda ()
-                             (interactive)
-                             (exwm-switch-to-buffer-or-run ,window-class ,command))))
-
     (spacemacs/exwm-bind-command
      "<s-return>"  exwm--terminal-command)
 
