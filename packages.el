8aa6c1cce9c6943b81ea58bbb0af7123f372e64f
timor <timor.dd@googlemail.com>
2020-01-02 14:17:30 +0100

    setting number of workspaces should be left to customize

--- packages.el
+++ packages.el
@@ -32,8 +32,6 @@
     :init
     ;; Disable dialog boxes since they are unusable in EXWM
     (setq use-dialog-box nil)
-    ;; 10 Worskpaces please
-    (setq exwm-workspace-number 1)
     ;; You may want Emacs to show you the time
     (display-time-mode t)
     (when exwm--hide-tiling-modeline
